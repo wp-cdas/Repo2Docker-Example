d2534a4a633f52dbe34e84c458ffbb365f91da43
Bryan Jonas <53985683+bryanjonas@users.noreply.github.com>
2019-11-05 14:08:59 -0500

    Delete install.R

--- install.R
+++
@@ -1,4 +0,0 @@
-#Use this file to specify an R/RStudio environment.
-
-#Here is an example of installing a package (remove the octothorp to actually install the package):
-#install.packages("tidyverse")
